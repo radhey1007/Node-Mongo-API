46aaec0ad7fe1356034e31e00cc41811e94ef359
villagerboy <ankitshukla1992@outlook.com>
2019-05-14 17:58:12 +0530

    source cleanup

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,10 +1,4 @@
-// import express from 'express';
-// //import { createUser } from '../controllers/userc';
 var createUser = require('../controllers/userc');
-// const router = express.Router();
-// router.post('/register', createUser);
-// export default router;
-
 module.exports = {
     //set up route configuration that will be handle by express server   
     configure: function (app) {
